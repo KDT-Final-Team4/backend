c70ee7452e48b747f685089a7c4404538d4df9f4
mystagogy <hi7747@naver.com>
2023-03-21 19:02:17 +0900

    #4 feat : 사용자 dto inner class 생성

---
+++ salarying/src/main/java/com/example/salarying/Corporation/User/dto/MemberDTO.java
@@ -0,0 +1,33 @@
+package com.example.salarying.Corporation.User.dto;
+
+import com.example.salarying.Corporation.User.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class MemberDTO {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SignUpRequest{
+        private String email;
+        private String password;
+        private String companyNm;
+        private String companyTel;
+
+        public Member toEntity(){
+            return Member.builder()
+                    .userEmail(this.email)
+                    .userPw(this.password)
+                    .companyNm(this.companyNm)
+                    .companyTel(this.companyTel)
+                    .role("USER")
+                    .build();
+        }
+    }
+
+
+}
