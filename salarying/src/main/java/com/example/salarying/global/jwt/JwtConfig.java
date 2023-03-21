bff6d65373a0d817acec738f3bd478519e14aaf8
mystagogy <hi7747@naver.com>
2023-03-21 20:38:07 +0900

    #8 refactor : Configuration Annotation 추가

--- salarying/src/main/java/com/example/salarying/global/jwt/JwtConfig.java
+++ salarying/src/main/java/com/example/salarying/global/jwt/JwtConfig.java
@@ -3,7 +3,9 @@ package com.example.salarying.global.jwt;
 import com.example.salarying.global.jwt.auth.AuthTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JwtConfig {
 
     @Value("${jwt.secretKey}")
