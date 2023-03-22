4e9461e93a2671742a6366d2848b1a37461d0451
mystagogy <hi7747@naver.com>
2023-03-22 14:12:06 +0900

    #13 refactor : @RequestBody 설정

--- salarying/src/main/java/com/example/salarying/Admin/User/controller/AdminLoginController.java
+++ salarying/src/main/java/com/example/salarying/Admin/User/controller/AdminLoginController.java
@@ -6,6 +6,7 @@ import com.example.salarying.global.dto.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,7 @@ public class AdminLoginController {
 
     @Operation(summary = "관리자 로그인 API", description = "이메일과 비밀번호로 로그인하기")
     @PostMapping("/admin/login")
-    public ResponseDTO<?> login(AdminDTO.LoginRequest request){
+    public ResponseDTO<?> login(@RequestBody AdminDTO.LoginRequest request){
         return new ResponseDTO<>().ok(adminService.login(request), "로그인 성공");
     }
 }
