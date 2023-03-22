deabbd2f4490f34152d88b1eb790547b73c85861
mystagogy <hi7747@naver.com>
2023-03-22 14:21:49 +0900

    #13 feat : swagger test default 설정 추가

--- salarying/src/main/java/com/example/salarying/Admin/User/dto/AdminDTO.java
+++ salarying/src/main/java/com/example/salarying/Admin/User/dto/AdminDTO.java
@@ -1,5 +1,6 @@
 package com.example.salarying.Admin.User.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,12 @@ public class AdminDTO {
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
+    @Schema(name = "관리자 로그인 DTO", description = "관리자 로그인시 입력받는 데이터")
     public static class LoginRequest{
+
+        @Schema(name = "email", example = "admin@email.com")
         private String email;
+        @Schema(name = "password", example = "admin@1234")
         private String password;
     }
 }
