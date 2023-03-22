e6f8155c052f0adf6c9606c5b864f91457db0a16
mystagogy <hi7747@naver.com>
2023-03-22 13:36:58 +0900

    #13 feat : admin service 생성

---
+++ salarying/src/main/java/com/example/salarying/Admin/User/service/AdminServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.salarying.Admin.User.service;
+
+import com.example.salarying.Admin.User.dto.AdminDTO;
+import com.example.salarying.Admin.User.entity.Admin;
+import com.example.salarying.Admin.User.repository.AdminRepository;
+import com.example.salarying.Corporation.User.exception.UserException;
+import com.example.salarying.Corporation.User.exception.UserExceptionType;
+import com.example.salarying.global.jwt.auth.AuthToken;
+import com.example.salarying.global.jwt.auth.AuthTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AdminServiceImpl implements AdminService{
+
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthTokenProvider authTokenProvider;
+
+
+    @Override
+    public String login(AdminDTO.LoginRequest request) {
+        Admin admin = adminRepository.findByAdminEmail(request.getEmail())
+                .orElseThrow(() -> new UserException(UserExceptionType.NOT_EXIST_ACCOUNT));
+
+        if(passwordEncoder.matches(request.getPassword(),admin.getAdminPw())){
+            AuthToken authToken = authTokenProvider.issueAdminAccessToken(admin);
+            return authToken.getToken();
+        }else throw new UserException(UserExceptionType.UNMATCHED_PASSWORD);
+    }
+}
