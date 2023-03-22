2eae5bf8ac8a754115fbe0ad9e3166d4c3560097
hamings <wjdgpal9630@gmail.com>
2023-03-22 17:10:47 +0900

    #10 feat: progress repository 생성

---
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/repository/ProgressRepository.java
@@ -0,0 +1,8 @@
+package com.example.salarying.Corporation.Recruiting.repository;
+
+
+import com.example.salarying.Corporation.Recruiting.entity.Progress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgressRepository extends JpaRepository<Progress,Long> {
+}
