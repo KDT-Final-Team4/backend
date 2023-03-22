7e56b341d74831947670427d579002592c1baa99
hamings <wjdgpal9630@gmail.com>
2023-03-22 14:15:45 +0900

    #14 fix: 로그인 기업id 확인 추가

--- salarying/src/main/java/com/example/salarying/Corporation/Recruiting/controller/ApplicantController.java
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/controller/ApplicantController.java
@@ -4,8 +4,10 @@ package com.example.salarying.Corporation.Recruiting.controller;
 import com.example.salarying.Corporation.Recruiting.dto.ApplicantDTO;
 import com.example.salarying.Corporation.Recruiting.service.ApplicantService;
 import com.example.salarying.global.dto.ResponseDTO;
+import com.example.salarying.global.jwt.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,8 @@ public class ApplicantController {
 
     @Operation(summary = "지원자 리스트 출력")
     @GetMapping("/applicants")
-    public ResponseDTO<?> showApplicantList(@RequestParam("recruiting_id") Long id){
-        List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantService.findApplicantByRecruitingId(id);
+    public ResponseDTO<?> showApplicantList(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("recruiting_id") Long id){
+        List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantService.findApplicantByRecruitingId(customUserDetails.getUserId(),id);
         return new ResponseDTO<>().ok(applicantResponseList,"정상출력 데이터");
     }
 }
