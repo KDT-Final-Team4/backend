87673706739b14c4cdff897dae179cc0fce61cd8
charcoal <devsj3586@gmail.com>
2023-03-23 19:38:32 +0900

    #2 feat : repository 생성

---
+++ salarying/src/main/java/com/example/salarying/Admin/Community/repository/NoticeRepository.java
@@ -0,0 +1,7 @@
+package com.example.salarying.Admin.Community.repository;
+
+import com.example.salarying.Admin.Community.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
