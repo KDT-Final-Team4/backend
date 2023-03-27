0e14e82729a0c0b56942caaa179ad2dcb499109b
charcoal <devsj3586@gmail.com>
2023-03-27 20:00:04 +0900

    #23 feat : 공지사항 상세 보기, 리스트 출력 기능 구현 추가

--- salarying/src/main/java/com/example/salarying/Admin/Community/controller/NoticeController.java
+++ salarying/src/main/java/com/example/salarying/Admin/Community/controller/NoticeController.java
@@ -7,9 +7,9 @@ import com.example.salarying.global.jwt.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +18,7 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     /**
-     * 관라지 id로 공지사항 등록 API
+     * 관리자 id로 공지사항 등록 API
      * @param userDetails : 로그인한 관리자
      * @param request     : 등록할 공지사항 정보 DTO
      * @return : 등록된 공지사항 DTO
@@ -28,5 +28,27 @@ public class NoticeController {
     public ResponseDTO<?> insertNotice(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody NoticeDTO.NoticeRequest request) {
         NoticeDTO.NoticeResponse responseDTO = noticeService.insertNotice(userDetails.getUserId(), request);
         return new ResponseDTO<>().ok(responseDTO, "공지사항 등록 완료");
+    }
+    /**
+     * 공지사항 목록 조회 API
+     * @return : 공지사항 전체 목록
+     */
+    @Operation(summary = "공지사항 리스트 조회")
+    @GetMapping("/notice")
+    public ResponseDTO<?> noticeList() {
+        List<NoticeDTO.NoticeList> noticeResponse = noticeService.noticeList();
+        return new ResponseDTO<>().ok(noticeResponse, "정상 출력");
+    }
+
+    /**
+     * 공지사항 정보 조회 API
+     * @param id : 공지사항 id
+     * @return : 공지사항 id를 포함한 공지사항 상세 정보 + "정상 출력"
+     */
+    @Operation(summary = "공지사항 정보 조회")
+    @GetMapping("/notice/{id}")
+    public ResponseDTO<?> noticeDetail(@RequestParam("id") Long id) {
+        NoticeDTO.NoticeResponse response = noticeService.noticeDetail(id);
+        return new ResponseDTO<>().ok(response, "정상출력");
     }
 }
