98b0e8bd69b8a74f752e7f579763fb5603ab1406
hamings <wjdgpal9630@gmail.com>
2023-03-28 21:03:02 +0900

    #44 fix: 지원자 리스트 출력 예외처리 수정

--- salarying/src/main/java/com/example/salarying/Corporation/Recruiting/service/impl/ApplicantServiceImpl.java
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/service/impl/ApplicantServiceImpl.java
@@ -2,12 +2,17 @@ package com.example.salarying.Corporation.Recruiting.service.impl;
 
 
 import com.example.salarying.Corporation.Recruiting.dto.ApplicantDTO;
+import com.example.salarying.Corporation.Recruiting.entity.Recruiting;
+import com.example.salarying.Corporation.Recruiting.exception.RecruitingException;
+import com.example.salarying.Corporation.Recruiting.exception.RecruitingExceptionType;
 import com.example.salarying.Corporation.Recruiting.repository.ApplicantRepository;
+import com.example.salarying.Corporation.Recruiting.repository.RecruitingRepository;
 import com.example.salarying.Corporation.Recruiting.service.ApplicantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -15,6 +20,8 @@ import java.util.stream.Collectors;
 public class ApplicantServiceImpl implements ApplicantService {
 
     private final ApplicantRepository applicantRepository;
+
+    private final RecruitingRepository recruitingRepository;
 
     /**
      * 기업id와 채용공고id가 일치하는 지원자리스트 출력
@@ -24,11 +31,16 @@ public class ApplicantServiceImpl implements ApplicantService {
      */
     @Override
     public List<ApplicantDTO.ApplicantResponse> findApplicantByRecruitingId(Long userId, Long recruitingId) {
-        List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantRepository.findApplicantByRecruitingId(recruitingId)
-                                                                                            .stream().filter(en->en.getRecruiting().getMember().getId()==userId)
-                                                                                                .map(entity->new ApplicantDTO.ApplicantResponse(entity))
-                                                                                                .collect(Collectors.toList());
-        return applicantResponseList;
+        Optional<Recruiting> recruitingOptional = recruitingRepository.findById(recruitingId);
+        if(recruitingOptional.isPresent()){
+            List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantRepository.findApplicantByRecruitingId(recruitingId)
+                    .stream().filter(en -> en.getRecruiting().getMember().getId() == userId)
+                    .map(entity -> new ApplicantDTO.ApplicantResponse(entity))
+                    .collect(Collectors.toList());
+            return applicantResponseList;
+        }else{
+            throw new RecruitingException(RecruitingExceptionType.NOT_EXIST);
+        }
     }
 
 
