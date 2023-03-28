62a0c915c4a96c305e1deb8a37223bfd1978c848
charcoal <devsj3586@gmail.com>
2023-03-28 17:13:23 +0900

    #37 feat : update 메서드 추가

--- salarying/src/main/java/com/example/salarying/Admin/Community/entity/Notice.java
+++ salarying/src/main/java/com/example/salarying/Admin/Community/entity/Notice.java
@@ -1,5 +1,7 @@
 package com.example.salarying.Admin.Community.entity;
 
+import com.example.salarying.Admin.Community.exception.CommunityException;
+import com.example.salarying.Admin.Community.exception.CommunityExceptionType;
 import com.example.salarying.Admin.User.entity.Admin;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
@@ -41,4 +43,19 @@ public class Notice {
 
     @Column(name = "status")
     private Boolean status;
+
+    public void update(String title, String content, boolean status) {
+        if (title.equals("")) {
+            throw new CommunityException(CommunityExceptionType.NOT_EXIST_TITLE);
+        } else {
+            this.title = title;
+        }
+        if (content.equals("")) {
+            throw new CommunityException(CommunityExceptionType.NOT_EXIST_CONTENT);
+        } else {
+            this.content = content;
+        }
+        this.status = status;
+    }
+
 }
