e2b3303f3669dbea4ec33ed68b0c68b98de7abd2
hamings <wjdgpal9630@gmail.com>
2023-03-30 18:00:03 +0900

    #62 comment: 주석 추가

--- salarying/src/main/java/com/example/salarying/Corporation/Recruiting/service/impl/ProgressServiceImpl.java
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/service/impl/ProgressServiceImpl.java
@@ -16,6 +16,12 @@ public class ProgressServiceImpl implements ProgressService {
 
     private final ProgressRepository progressRepository;
 
+    /**
+     * 채용공고 id와 기업 id로 채용전형 출력
+     * @param userid: 기업 id
+     * @param recruitingId: 채용공고 id
+     * @return 해당 채용공고 전형단계
+     */
     @Override
     public ProgressDTO.ProgressResponse findProgressRecruitingIdAndUserId(Long userid, Long recruitingId) {
         Progress progress = progressRepository.findProgressByRecruiting_IdAndRecruiting_Member_Id(recruitingId,userid)
