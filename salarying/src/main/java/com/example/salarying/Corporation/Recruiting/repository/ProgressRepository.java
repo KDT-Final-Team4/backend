baa1aa64eaf70a691f5d7835069beab575dde5d1
hamings <wjdgpal9630@gmail.com>
2023-03-30 17:55:55 +0900

    #62 feat: 채용공고id와 기업id로 progress 찾는 함수

--- salarying/src/main/java/com/example/salarying/Corporation/Recruiting/repository/ProgressRepository.java
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/repository/ProgressRepository.java
@@ -4,5 +4,9 @@ package com.example.salarying.Corporation.Recruiting.repository;
 import com.example.salarying.Corporation.Recruiting.entity.Progress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProgressRepository extends JpaRepository<Progress,Long> {
+
+    Optional<Progress> findProgressByRecruiting_IdAndRecruiting_Member_Id(Long recruitingId,Long userId);
 }
