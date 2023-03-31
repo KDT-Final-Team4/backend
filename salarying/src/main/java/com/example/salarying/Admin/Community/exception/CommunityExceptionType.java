dd7ad07fc581bc35cb3bfd29f74648d8287c828b
charcoal <devsj3586@gmail.com>
2023-03-31 15:22:48 +0900

    #57 feat : 커뮤니티 예외 타입 추가

--- salarying/src/main/java/com/example/salarying/Admin/Community/exception/CommunityExceptionType.java
+++ salarying/src/main/java/com/example/salarying/Admin/Community/exception/CommunityExceptionType.java
@@ -5,11 +5,12 @@ import org.springframework.http.HttpStatus;
 
 public enum CommunityExceptionType implements CustomExceptionType {
 
-    NOT_EXIST_TITLE(-301, HttpStatus.BAD_REQUEST, "공지사항 제목이 입력되지 않았습니다."),
-    NOT_EXIST_CONTENT(-302, HttpStatus.BAD_REQUEST, "공지사항 내용이 입력되지 않았습니다."),
+    NOT_EXIST_TITLE(-301, HttpStatus.BAD_REQUEST, "제목이 입력되지 않았습니다."),
+    NOT_EXIST_CONTENT(-302, HttpStatus.BAD_REQUEST, "내용이 입력되지 않았습니다."),
     NOT_EXIST_NOTICE(-303, HttpStatus.BAD_REQUEST, "공지사항이 없습니다."),
-
-    ;
+    NOT_EXIST_QUESTION(-304, HttpStatus.BAD_REQUEST, "질문이 입력되지 않았습니다."),
+    NOT_EXIST_ANSWER(-305, HttpStatus.BAD_REQUEST, "답변이 입력되지 않았습니다."),
+    NOT_EXIST_CATEGORY(-306, HttpStatus.BAD_REQUEST, "카테고리가 입력되지 않았습니다.");
 
     private final int errorCode;
     private final HttpStatus httpStatus;
