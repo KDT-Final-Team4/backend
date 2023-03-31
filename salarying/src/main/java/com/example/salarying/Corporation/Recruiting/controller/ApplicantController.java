d93ffa0d8e43023c5a734e8f49768a70620df765
hamings <wjdgpal9630@gmail.com>
2023-03-31 18:23:29 +0900

    #79 comment: description 추가

--- salarying/src/main/java/com/example/salarying/Corporation/Recruiting/controller/ApplicantController.java
+++ salarying/src/main/java/com/example/salarying/Corporation/Recruiting/controller/ApplicantController.java
@@ -25,7 +25,7 @@ public class ApplicantController {
      * @param id: 채용공고 id
      * @return: 기업의 채용공고별 지원자 리스트 출력
      */
-    @Operation(summary = "지원자 리스트 출력")
+    @Operation(summary = "지원자 리스트 출력", description = "지원자 리스트 출력 FOR USER")
     @GetMapping("/applicants")
     public ResponseDTO<?> showApplicantList(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("recruiting_id") Long id){
         List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantService.findApplicantByRecruitingId(customUserDetails.getUserId(),id);
@@ -38,7 +38,7 @@ public class ApplicantController {
      * @param request: 채용공고id,채용전형,합격여부 정보를 가진 DTO
      * @return: 조건에 해당하는 지원자 리스트
      */
-    @Operation(summary = "채용전형과 합격여부와 일치하는 지원자 리스트 출력")
+    @Operation(summary = "채용전형과 합격여부와 일치하는 지원자 리스트 출력", description = "채용전형과 합격여부와 일치하는 지원자 리스트 출력 FOR USER")
     @PostMapping("/applicants/selection")
     public ResponseDTO<?> showSelectedApplicantList(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ApplicantDTO.SelectionRequest request){
         List<ApplicantDTO.ApplicantResponse> applicantResponseList = applicantService.findApplicantByRecruitingIdAndProgressAndStatus(customUserDetails.getUserId(), request);
@@ -51,7 +51,7 @@ public class ApplicantController {
      * @param request: 지원자 정보 담은 DTO
      * @return 등록된 지원자 정보
      */
-    @Operation(summary = "채용공고에 지원자 등록")
+    @Operation(summary = "채용공고에 지원자 등록", description = "지원자 등록 FOR USER")
     @PostMapping("/applicants")
     public ResponseDTO<?> insertApplicant(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ApplicantDTO.ApplicantRequest request){
         ApplicantDTO.ApplicantResponse applicantResponse = applicantService.insertApplicant(customUserDetails.getUserId(), request);
@@ -64,7 +64,7 @@ public class ApplicantController {
      * @param request: 수정할 내용 담은 DTO
      * @return: 수정된 지원자 정보
      */
-    @Operation(summary = "지원자 progress,status값 수정")
+    @Operation(summary = "지원자 progress,status값 수정",description = "전형단계, 합격여부 값 변경 FOR USER")
     @PutMapping("/applicants")
     public ResponseDTO<?> updateStatus(@AuthenticationPrincipal CustomUserDetails customUserDetails, ApplicantDTO.ResultRequest request){
         ApplicantDTO.ApplicantResponse applicantResponse = applicantService.updateApplicant(customUserDetails.getUserId(),request);
